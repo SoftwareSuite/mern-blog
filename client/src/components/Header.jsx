8e8480f6aaab5e5f594f81d6a70c40bf6dffe64a
Usman Ghori <usman.ghori1@icloud.com>
2024-07-20 08:59:30 -0700

    create server using express

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
           <FaMoon/>
         </Button>
         <Link to='/sign-in'>
-        <Button gradientDuoTone='purpleToBlue'>
+        <Button gradientDuoTone='purpleToPink' outline>
           Sign In
         </Button>
         </Link>
