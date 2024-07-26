bd3e50f0682651c05b7d8f4b8d44553b0bf30c2c
Usman Ghori <usman.ghori1@icloud.com>
2024-07-25 23:54:01 -0700

    add OAuth functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,6 +16,10 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true, 
+    },
+    profilePicture: {
+        type: String,
+        default: "https://wallpapers.com/images/featured/cool-profile-pictures-4co57dtwk64fb7lv.jpg",
     }
 },
 {timestamps: true});
