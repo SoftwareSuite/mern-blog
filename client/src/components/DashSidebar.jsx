786e7992fac5ab05fef0309f8cf7bd8520bb2224
Usman Ghori <usman.ghori1@icloud.com>
2024-07-31 00:03:26 -0700

    complete profile page UI

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -17,7 +17,7 @@ useEffect(() =>{
   return (
     <Sidebar className='w-full md:w-56'>
         <Sidebar.Items>
-            <Sidebar.ItemGroup>
+            <Sidebar.ItemGroup className='flex flex-col gap-1'>
                 <Link to='/dashboard?tab=profile'>
                 <Sidebar.Item active={tab === 'profile'} icon={HiUser} label={'User'} lableColor="dark">
                     Profile
